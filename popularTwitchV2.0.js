f4a50a9604c9ede56f5894d31c1b8b176d71722a
BoyanP <bpeychof@ualberta.ca>
2017-09-03 01:17:36 -0600

    just needed to update the queries with a client id

--- popularTwitchV2.0.js
+++ popularTwitchV2.0.js
@@ -13,25 +13,27 @@ $(document).ready(function(){
     $scope.games;
     $scope.streamers = [];
     
-    $http.get("https://api.twitch.tv/kraken/games/top").success(function(topGames){
+    $http.get("https://api.twitch.tv/kraken/games/top?client_id=swheeu8yk5hiqgcgo69m58amq5ynn5").success(function(topGames){
       
-      //console.log(topGames);
+      console.log(topGames);
       
       $scope.games = topGames.top;
       $scope.count=0;
 
-     //console.log($scope.games[0].game.name);
+     console.log($scope.games[0].game.name);
      var gameQuery;
      $scope.games.forEach(function(twitchGame,index){
        gameQuery = twitchGame.game.name.replace(/\s/g,"+");
+       console.log();
 			 gameQuery = gameQuery.replace("&","%26");// twitch api doesn't like having & in there. 
 																								// it doesn't complain about : though
        
        //console.log($scope.count);
-       $http.get("https://api.twitch.tv/kraken/streams?game="+gameQuery).success(function(streamers){
+       $http.get("https://api.twitch.tv/kraken/streams?client_id=swheeu8yk5hiqgcgo69m58amq5ynn5&game="+gameQuery).success(function(streamers){
         
          $scope.streamers.push({game:streamers.streams[0].game,
                                 streams:streamers.streams.slice(0,3)});
+         console.log($scope.streamers);
          
        });
       //console.log($scope.streamers[0].game);
